69a68b3c49354ff311825ec065eb5efb5e163745
thiennguyendev <thiennguyen.mark@gmail.com>
2018-10-10 21:36:59 +0700

    This is first commit

---
+++ public/app.js
@@ -0,0 +1,126 @@
+(function(){
+  // Inicia o firebase Firebase
+  var config = {
+   apiKey: "AIzaSyCzzIBg1u5XhWjye3Hu58AboXYaN7l8Cpk",
+   authDomain: "thiennguyen-phantan.firebaseapp.com",
+   databaseURL: "https://thiennguyen-phantan.firebaseio.com",
+   projectId: "thiennguyen-phantan",
+   storageBucket: "thiennguyen-phantan.appspot.com",
+   messagingSenderId: "631921135943"
+ };
+
+ firebase.initializeApp(config);
+
+  var db = firebase.database();
+
+  // Cria os listeners dos dados no firebase
+  var tempRef = db.ref('temperature');
+  var umidRef = db.ref('humidity');
+  var presenceRef = db.ref('presence');
+  var lampRef = db.ref('lamp');
+
+  // Registra as funções que atualizam os gráficos e dados atuais da telemetria
+  tempRef.on('value', onNewData('currentTemp', 'tempLineChart' , 'Temperatura', 'C°'));
+  umidRef.on('value', onNewData('currentUmid', 'umidLineChart' , 'Umidade', '%'));
+
+
+  // Registrar função ao alterar valor de presença
+  presenceRef.on('value', function(snapshot){
+    var value = snapshot.val();
+    var el = document.getElementById('currentPresence')
+    if(value){
+      el.classList.add('green-text');
+    }else{
+      el.classList.remove('green-text');
+    }
+  });
+
+  // Registrar função ao alterar valor da lampada
+  var currentLampValue = false;
+  lampRef.on('value', function(snapshot){
+    var value = snapshot.val();
+    var el = document.getElementById('currentLamp')
+    if(value){
+      el.classList.add('amber-text');
+    }else{
+      el.classList.remove('amber-text');
+    }
+    currentLampValue = !!value;
+  });
+
+  // Registrar função de click no botão de lampada
+  var btnLamp = document.getElementById('btn-lamp');
+  btnLamp.addEventListener('click', function(evt){
+    lampRef.set(!currentLampValue);
+  });
+
+})();
+
+
+// Retorna uma função que de acordo com as mudanças dos dados
+// Atualiza o valor atual do elemento, com a metrica passada (currentValueEl e metric)
+// e monta o gráfico com os dados e descrição do tipo de dados (chartEl, label)
+function onNewData(currentValueEl, chartEl, label, metric){
+  return function(snapshot){
+    var readings = snapshot.val();
+    if(readings){
+        var currentValue;
+        var data = [];
+        for(var key in readings){
+          currentValue = readings[key]
+          data.push(currentValue);
+        }
+
+        document.getElementById(currentValueEl).innerText = currentValue + ' ' + metric;
+        buildLineChart(chartEl, label, data);
+    }
+  }
+}
+
+// Constroi um gráfico de linha no elemento (el) com a descrição (label) e os
+// dados passados (data)
+function buildLineChart(el, label, data){
+  var elNode = document.getElementById(el);
+  new Chart(elNode, {
+    type: 'line',
+    data: {
+        labels: new Array(data.length).fill(""),
+        datasets: [{
+            label: label,
+            data: data,
+            borderWidth: 1,
+            fill: false,
+            spanGaps: false,
+            lineTension: 0.1,
+            backgroundColor: "#F9A825",
+            borderColor: "#F9A825"
+        }]
+    }
+  });
+}
+
+// function guid() {
+//   function s4() {
+//     return Math.floor((1 + Math.random()) * 0x10000)
+//       .toString(16)
+//       .substring(1);
+//   }
+//   return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+// }
+//
+// var db = firebase.database();
+// var ref = db.ref("temperature");
+//
+// setInterval(function(){
+//   data = {};
+//
+//   data[guid()] = 100 * (Math.random()).toFixed(2);
+//   data[guid()] = 100 * (Math.random()).toFixed(2);
+//   data[guid()] = 100 * (Math.random()).toFixed(2);
+//   data[guid()] = 100 * (Math.random()).toFixed(2);
+//   data[guid()] = 100 * (Math.random()).toFixed(2);
+//
+//   ref.set(data);
+//
+//   console.log('----->', data);
+// }, 2000)
